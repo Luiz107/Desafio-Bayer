f935630b3762a1f796c44871c0387d4bd10d1b01
luiz gabriel bruscagim <luizgabriel107@hotmail.com>
2023-11-19 22:02:47 -0300

    Add Commit

---
+++ Aplicacao/enterprise-challenge-app/src/bean/Doenca.java
@@ -0,0 +1,31 @@
+package bean;
+
+public class Doenca {
+    private int idDoenca;
+    private String nmDoenca;
+
+    public Doenca(int idDoenca, String nmDoenca) {
+        this.idDoenca = idDoenca;
+        this.nmDoenca = nmDoenca;
+    }
+
+    public Doenca() {
+
+    }
+
+    public int getIdDoenca() {
+        return idDoenca;
+    }
+
+    public void setIdDoenca(int idDoenca) {
+        this.idDoenca = idDoenca;
+    }
+
+    public String getNmDoenca() {
+        return nmDoenca;
+    }
+
+    public void setNmDoenca(String nmDoenca) {
+        this.nmDoenca = nmDoenca;
+    }
+}
---
+++ Aplicacao/enterprise-challenge-app/src/bean/Zona.java
@@ -0,0 +1,31 @@
+package bean;
+
+public class Zona {
+    private int idZona;
+    private String nmZona;
+
+    public Zona(int idZona, String nmZona) {
+        this.idZona = idZona;
+        this.nmZona = nmZona;
+    }
+
+    public Zona() {
+
+    }
+
+    public int getIdZona() {
+        return idZona;
+    }
+
+    public void setIdZona(int idZona) {
+        this.idZona = idZona;
+    }
+
+    public String getNmZona() {
+        return nmZona;
+    }
+
+    public void setNmZona(String nmZona) {
+        this.nmZona = nmZona;
+    }
+}
